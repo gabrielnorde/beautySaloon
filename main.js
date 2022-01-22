36066578756cf10be9c4f7bd637847a47807de04
gabrielnorde <gabrielnorde@gmail.com>
2022-01-22 11:53:35 -0300

    Site mobile first

---
+++ main.js
@@ -0,0 +1,60 @@
+// Abre e fecha o menu através dos ícones
+
+const nav = document.querySelector(' #header nav')
+let toggle = document.querySelectorAll('nav .toggle')
+
+for (let element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// quando clicar em um item do menu, esconder o menu
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// mudar header da página quando der scroll
+const header = document.querySelector(' #header ')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+// Testimonials caroussel
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// ScrollReveal: mostrar elementos quando o usuário der scroll na página
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`,
+  { interval: 100 }
+)
